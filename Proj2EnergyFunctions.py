5a09958625407a4922b8807ff2b4643fe8a21ea5
Eli-Lin-7 <elinmail77@gmail.com>
2025-11-04 18:37:25 -0500

    Add files via upload

---
+++ Proj2EnergyFunctions.py
@@ -0,0 +1,18 @@
+import math as m
+GRAVITY = 9.81
+
+def calcFluidSpeed(flowRate, radius):
+    return pow(flowRate / (m.pi * pow(radius, 2)), 2)
+
+def pipeFriction(density, time, frictionFactor, flowRate, velocity, length, diameter):
+    mass = density * flowRate * time
+    headLoss = frictionFactor * 8 * (flowRate ** 2) * length / ((m.pi ** 2) * GRAVITY * pow(diameter, 5))
+    energyLoss = mass * headLoss * GRAVITY
+    return energyLoss
+
+def bendLoss(bendFactor, fluidSpeed):
+    energyLoss = bendFactor * (fluidSpeed ** 2) / (2 * GRAVITY)
+    return energyLoss
+
+def valveLoss(resistanceCoefficent, fluidSpeed):
+    return (pow(fluidSpeed, 2) * resistanceCoefficent / (2 * GRAVITY))
