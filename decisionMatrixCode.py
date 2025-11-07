521c23d48fe9a11c4f78bcab13ae9a7c754f43d9
Eli-Lin-7 <elinmail77@gmail.com>
2025-11-07 14:59:28 -0500

    Add files via upload

---
+++ decisionMatrixCode.py
@@ -0,0 +1,24 @@
+#Takes in site, cost, effiency as well as the maxCost and maxEfficiency
+#Most likely will need to run code with all three sites to get maxCost/efficiency
+def getDecisionMatrixValue(site, cost, efficiency, maxCost, maxEfficiency):
+    normalizedCost = 0
+    normalizedEfficiency = 0
+    
+    if(site == 1):
+        cost += 6660000
+        normalizedCost = (1 - (cost / maxCost))
+        
+        normalizedEfficiency = efficiency / maxEfficiency
+    elif(site == 2):
+        cost += 9300000
+        normalizedCost = (1 - (cost / maxCost))
+        
+        normalizedEfficiency = efficiency / maxEfficiency
+    elif(site == 3):
+        cost += 1616500 
+        normalizedCost = (1 - (cost / maxCost))
+        
+        normalizedEfficiency = efficiency / maxEfficiency
+    
+    #Returns the weighted normalized cost and efficiency
+    return [0.4 * normalizedCost, 0.4 * normalizedEfficiency]
