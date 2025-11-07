db9f9cb72b0e112f4383d8c48a9ae6aab943216f
Eli-Lin-7 <elinmail77@gmail.com>
2025-11-07 15:50:20 -0500

    Add files via upload

--- decisionMatrixCode.py
+++ decisionMatrixCode.py
@@ -3,25 +3,33 @@
 def getDecisionMatrixValue(site, cost, efficiency):
     normalizedCost = 0
     normalizedEfficiency = 0
+    normalizedEthical= 0
     
     maxEfficiency = 15
-    maxCost = 10000000
+    maxCost = 20000000
+    maxEthical = 20
     
     if(site == 1):
         cost += 6660000
         normalizedCost = (1 - (cost / maxCost))
         
         normalizedEfficiency = efficiency / maxEfficiency
+        normalizedEthical = 1 - (20 / maxEthical)
     elif(site == 2):
         cost += 9300000
         normalizedCost = (1 - (cost / maxCost))
         
         normalizedEfficiency = efficiency / maxEfficiency
+        normalizedEthical = 1 - (0 / maxEthical) 
     elif(site == 3):
         cost += 1616500 
         normalizedCost = (1 - (cost / maxCost))
         
         normalizedEfficiency = efficiency / maxEfficiency
+        normalizedEthical = 1 - (0 / maxEthical)
     
-    #Returns the weighted normalized cost and efficiency
-    return [0.4 * normalizedCost, 0.4 * normalizedEfficiency]
+    #Returns three seperate values as array
+    #return [0.4 * normalizedCost, 0.4 * normalizedEfficiency, 0.2 * normalizedEthical]
+    
+    #Returns one value
+    return ((0.4 * normalizedCost) + (0.4 * normalizedEfficiency) + (0.4 * normalizedEthical))
