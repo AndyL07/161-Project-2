849ef19af9aafa18780177694ffe0c39d61857cf
Eli-Lin-7 <elinmail77@gmail.com>
2025-11-09 20:12:43 -0500

    Add files via upload

--- decisionMatrixCode.py
+++ decisionMatrixCode.py
@@ -1,14 +1,15 @@
-#Takes in site, cost, effiency as well as the maxCost and maxEfficiency
-#Most likely will need to run code with all three sites to get maxCost/efficiency
+#Takes in site, cost and efficiency
 def getDecisionMatrixValue(site, cost, efficiency):
     normalizedCost = 0
     normalizedEfficiency = 0
     normalizedEthical= 0
     
+    #intialize max values for each parameter
     maxEfficiency = 40
     maxCost = 20000000
     maxEthical = 20
     
+    #calculate decision matrix values based on site
     if(site == 1):
         cost += 6660000
         normalizedCost = (1 - (cost / maxCost))
@@ -31,7 +32,7 @@ def getDecisionMatrixValue(site, cost, efficiency):
     #Returns three seperate values as array
     #return [0.4 * normalizedCost, 0.4 * normalizedEfficiency, 0.2 * normalizedEthical]
    
-    # Returns one values    
+    # Returns total score for decision matrix  
     finalVal = ((0.4 * normalizedCost) + (0.4 * normalizedEfficiency) + (0.2 * normalizedEthical))
     if finalVal > 1:
         print(f"Cost: {normalizedCost:2.3f} --- Eff: {normalizedEfficiency:2.3f} --- Eth: {normalizedEthical:2.3f}")
